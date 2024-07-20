e939cae2c60061f37241b07b999f2ee043fa38ce
Vojtěch <kalivoda167@gmail.com>
2024-07-20 14:10:06 +0200

    Initial Commit

---
+++ IDK2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IDK2
+//
+//  Created by Vojtěch Kalivoda on 7/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IDK2/IDK2App.swift
@@ -0,0 +1,17 @@
+//
+//  IDK2App.swift
+//  IDK2
+//
+//  Created by Vojtěch Kalivoda on 7/20/24.
+//
+
+import SwiftUI
+
+@main
+struct IDK2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
